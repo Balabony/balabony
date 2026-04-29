9d41c220f03de5752c961c0edc8de3d98942dddc
Balabony <mksukr@gmail.com>
2026-04-29 19:44:06 +0300

    Remove API keys

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "79563a2da0f4888acb6debc53981ab69
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "9c87b484d807a4c69da48d4b5bc13dcb",
+"flutter_bootstrap.js": "ad7e63a564fc36053878ef7b77a0fa62",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
